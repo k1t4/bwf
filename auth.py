588ad04ca356f32cced579ee51c454448c883a0f
Nik Chebanov <kit4nikit4@gmail.com>
2022-01-16 20:40:35 +0300

    Add class Executor which will handle most of the work + refactoring

--- auth.py
+++ auth.py
@@ -16,16 +16,22 @@ def is_user_logged_in() -> bool:
     return status != 'unauthenticated'
 
 
-def get_session_token() -> str:
+def is_vault_locked():
+    """Checks if vault of logged-in user is locked or not"""
+    completed_ps = subprocess.run(['bw', 'status'], capture_output=True)
+    status = json.loads(completed_ps.stdout)['status']
+    return status == 'locked'
+
+
+def unlock_and_get_token() -> str:
     """Gets user's session token"""
     print('Please, submit your password:')
     passwd = getpass.getpass('Password: ')
-    completed_ps = subprocess.run(
-        ['bw', 'unlock', passwd], capture_output=True)
+    completed_ps = subprocess.run(['bw', 'unlock', passwd], capture_output=True)
     # check for errors
     if completed_ps.returncode == 1:
         print('Incorrect password, try again...')
-        get_session_token()
+        unlock_and_get_token()
     token = re.findall(r'BW_SESSION="(\S+)"', str(completed_ps.stdout))[0]
     return token
 
@@ -39,7 +45,7 @@ def remove_token():
     """Deletes token from environment"""
 
 
-def log_user_in() -> str:
+def log_in_and_get_token() -> str:
     """
     Logs user in and returns session token to environment,
     should be called only if user is not logged-in
@@ -48,17 +54,27 @@ def log_user_in() -> str:
     email = input('E-mail: ')
     passwd = getpass.getpass('Password: ')
     # need to ensure that it is safe
-    completed_ps = subprocess.run(
-        ['bw', 'login', email, passwd], capture_output=True)
+    completed_ps = subprocess.run(['bw', 'login', email, passwd])
     if completed_ps.returncode == 1:
         print('Incorrect credentials, try again...', end='\n\n')
-        log_user_in()
-    else:
-        token = re.findall(
-            r'BW_SESSION="(\S+)"', str(completed_ps.stdout))[0]
-        print('You are successfully logged-in')
-        return token
+        log_in_and_get_token()
+    token = re.findall(r'BW_SESSION="(\S+)"', str(completed_ps.stdout))[0]
+    print('You are successfully logged-in')
+    return token
 
 
 def log_user_out():
     """Logs user out"""
+
+
+def authenticate():
+    """
+    Authenticates user and unlocks vault with exporting session token
+    """
+    if not is_user_logged_in():
+        token = log_in_and_get_token()
+        export_token(token)
+    elif is_vault_locked():
+        token = unlock_and_get_token()
+        export_token(token)
+
--- core.py
+++ core.py
@@ -1,55 +1,92 @@
 """
 Core module where all commands are handled
 """
-import shutil
 import subprocess
 import json
+from collections import namedtuple
 
 
-def get(target, *args):
-    """Get action implementation"""
-    # fetching fresh data
-    subprocess.run(['bw', 'sync'])
-    if target == 'credentials':
-        if not args:
-            completed_ps = subprocess.run(
-                ['bw', 'list', 'items'], capture_output=True
-            )
-            return completed_ps.stdout
-        search_pattern = args[0]
-        completed_ps = subprocess.run(
-            ['bw', 'list', 'items', '--search', search_pattern]
-        )
-        return completed_ps.stdout
+class Executor:
+    """
+    A core class, that executes commands by fetching or writing data from
+    original bw-cli
+    """
+    def __init__(self, cl_args):
+        self.action = cl_args.action_name
+        self.result_list = None
 
+        if self.action == 'show':
+            self.search_pattern = cl_args.search_pattern
+            self.show_pass = cl_args.password
+            self.show_uname = cl_args.username
 
-def create(target):
-    pass
+        elif self.action == 'create':
+            self.item_name = cl_args.item_name
+            self.set_pass = cl_args.password
+            self.set_uname = cl_args.username
 
-def delete(target):
-    pass
-
-class Core:
-    def __init__(self):
-        ...
-
-    @staticmethod
-    def get(target, *args):
-        completed_ps = subprocess.run(
-            ['bw', 'list', 'items'], capture_output=True)
-        items = json.loads(completed_ps.stdout)
-        if target == 'password':
+        elif self.action == 'delete':
             pass
-        elif target == 'login':
-            bw_command = []
-        elif target == 'creds':
-            result = [(item['login']['username'], item['login']['password'])
-                      for item in items if args[0] in item['name'].lower()]
-            return result
 
-    def create(self, target, *args):
-        ...
+    def execute_command(self):
+        """Interface for actions execution"""
+        # fetching fresh data
+        subprocess.run(['bw', 'sync'], stdout=subprocess.DEVNULL)
+        if self.action == 'show':
+            self.execute_cmd_show()
+        if self.action == 'create':
+            self.execute_cmd_create()
+        if self.action == 'delete':
+            pass
 
-    def delete(self, target, *args):
-        ...
+    def execute_cmd_show(self):
+        """Generates a list of items to show based on a search_pattern"""
+        completed_ps = subprocess.run(
+            ['bw', 'list', 'items'], capture_output=True
+        )
+        items = json.loads(completed_ps.stdout)
+        CleanItem = namedtuple('CleanItem', 'name username password')
+        clean_items = []
+        for item in items:
+            clean_item = CleanItem(item['name'],
+                                   item['login']['username'],
+                                   item['login']['password'])
+            clean_items.append(clean_item)
+
+        if self.search_pattern:
+            clean_items = list(
+                filter(lambda i: self.search_pattern.lower() in i.name.lower(),
+                       clean_items))
+
+        self.result_list = clean_items
+
+    def execute_cmd_create(self):
+        """Creates a new record with credentials in a vault"""
+        pass
+
+    def execute_cmd_delete(self):
+        """Deletes existing record in a vault"""
+        pass
+
+    def pretty_print(self):
+        """Prints result of an executed action"""
+        if self.action == 'show':
+            output_s = ''
+            both_params = self.show_pass and self.show_uname
+            no_params = not (self.show_pass or self.show_uname)
+            if both_params or no_params:
+                for item in self.result_list:
+                    output_s += f'{item.name}:\n' \
+                                f'username: {item.username}\n' \
+                                f'password: {item.password}\n\n'
+            elif self.show_uname:
+                for item in self.result_list:
+                    output_s += f'{item.name}:\n' \
+                                f'username: {item.username}\n\n'
+            else:
+                for item in self.result_list:
+                    output_s += f'{item.name}:\n' \
+                                f'password: {item.password}\n\n'
+
+            print(output_s)
 
