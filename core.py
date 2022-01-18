8329f9e14f65e4ff830ce9ef1db50e69ef12bbb3
Nik Chebanov <kit4nikit4@gmail.com>
2022-01-18 20:01:22 +0300

    Added delete action + renamed some variables + refactored

--- core.py
+++ core.py
@@ -15,101 +15,129 @@ class Executor:
     """
     def __init__(self, cl_args):
         self.action = cl_args.action_name
-        self.result_list = None
+        self.result = None
 
         if self.action == 'show':
             self.search_pattern = cl_args.search_pattern
-            self.show_pass = cl_args.password
-            self.show_uname = cl_args.username
+            self.param_show_pass = cl_args.password
+            self.param_show_uname = cl_args.username
 
         elif self.action == 'create':
             self.item_name = cl_args.item_name
-            self.set_pass = cl_args.password
-            self.set_uname = cl_args.username
+            self.param_set_pass = cl_args.password
+            self.param_set_uname = cl_args.username
 
         elif self.action == 'delete':
-            pass
+            self.search_pattern = cl_args.search_pattern
 
     def execute_command(self):
         """Interface for actions execution"""
-        # fetching fresh data
-        sp.run(['bw', 'sync'], stdout=sp.DEVNULL)
+        sp.run(['bw', 'sync'], stdout=sp.DEVNULL)   # fetching fresh data
         if self.action == 'show':
             self.execute_cmd_show()
         if self.action == 'create':
             self.execute_cmd_create()
         if self.action == 'delete':
-            pass
+            self.execute_cmd_delete()
 
     def execute_cmd_show(self):
         """Generates a list of items to show based on a search_pattern"""
-        completed_ps = sp.run(['bw', 'list', 'items'], capture_output=True)
-        items = json.loads(completed_ps.stdout)
+        items = self.__get_items_by_search_pattern(self.search_pattern)
+
         CleanItem = namedtuple('CleanItem', 'name username password')
-        clean_items = []
-        for item in items:
-            clean_item = CleanItem(item['name'],
-                                   item['login']['username'],
-                                   item['login']['password'])
-            clean_items.append(clean_item)
+        clean_items = [
+            CleanItem(i['name'], i['login']['username'], i['login']['password'])
+            for i in items]
 
-        if self.search_pattern:
-            clean_items = list(
-                filter(lambda i: self.search_pattern.lower() in i.name.lower(),
-                       clean_items))
-
-        self.result_list = clean_items
+        self.result = clean_items
 
     def execute_cmd_create(self):
         """Creates a new record with credentials in a vault"""
         # getting templates and filling them with credentials
-        template_ps = sp.run(['bw', 'get', 'template', 'item'],
-                             capture_output=True)
-        template_item = json.loads(template_ps.stdout)
-        template_ps = sp.run(['bw', 'get', 'template', 'item.login'],
-                             capture_output=True)
-        template_item_login = json.loads(template_ps.stdout)
-        # both args are not set = both args are true
-        if not (self.set_pass or self.set_uname):
-            self.set_pass, self.set_uname = True, True
-        if self.set_uname:
+        template_item = self.__get_template('item')
+        template_item_login = self.__get_template('item.login')
+        # no args == both args
+        if not (self.param_set_pass or self.param_set_uname):
+            self.param_set_pass, self.param_set_uname = True, True
+        if self.param_set_uname:
             username = input('New username: ')
             template_item_login['username'] = username
-        if self.set_pass:
+        if self.param_set_pass:
             password = getpass.getpass('Password: ')
             template_item_login['password'] = password
 
         template_item['name'] = self.item_name
         template_item['login'] = template_item_login
 
-        # encoding and sending filled templates to bw creator
+        # encoding and sending filled templates to bw cli
         encoded_template = base64.b64encode(json.dumps(template_item).encode())
         creator_ps = sp.run(['bw', 'create', 'item'], input=encoded_template,
                             stdout=sp.DEVNULL)
         if creator_ps == 1:
             raise ChildProcessError('bw create has finished with an error')
-        print(f'Created item {self.item_name}')
+        self.result = f'Created item {self.item_name}'
 
     def execute_cmd_delete(self):
-        """Deletes existing record in a vault"""
-        pass
+        """Deletes existing record in a vault by search pattern"""
+        items = self.__get_items_by_search_pattern(self.search_pattern)
+        if not items:
+            print('No matching items, nothing to delete')
+            return None
+        elif len(items) == 1:
+            item_to_delete = items[0]
+        else:   # when multiple items match
+            print('Multiple items match your query,'
+                  ' please choose one to delete:')
+            for index, item in enumerate(items):
+                print(f'{index+1}) {item["name"]} with id {item["id"]}')
+            chosen_index = input('Type which one to delete (ex: 1): ')
+            while not chosen_index.isnumeric() \
+                    or not (1 <= int(chosen_index) <= len(items)):
+                chosen_index = input('Please, choose from integers: ')
+            chosen_index = int(chosen_index)
+            item_to_delete = items[int(chosen_index)-1]
+
+        deleter_ps = sp.run(['bw', 'delete', 'item', item_to_delete['id']],
+                            stdout=sp.DEVNULL)
+        if deleter_ps.returncode == 1:
+            raise ChildProcessError('bw delete has finished with an error')
+        self.result = f'Deleted item {item_to_delete["name"]}'
 
     def pretty_print(self):
         """Prints result of an executed action"""
         if self.action == 'show':
             output_s = ''
-            # no params
-            if not (self.show_pass or self.show_uname):
-                self.show_uname = True
-                self.show_pass = True
+            # no params == all params
+            if not (self.param_show_pass or self.param_show_uname):
+                self.param_show_uname, self.param_show_pass = True, True
 
-            for item in self.result_list:
+            for item in self.result:
                 output_s += f'{item.name}:\n'
-                if self.show_uname:
+                if self.param_show_uname:
                     output_s += f'username: {item.username}\n'
-                if self.show_pass:
+                if self.param_show_pass:
                     output_s += f'password: {item.password}\n'
                 output_s += '\n'
 
             print(output_s, end='')
+        else:
+            print(self.result)
 
+    @staticmethod
+    def __get_template(template_name):
+        """Just gets a template from bw cli"""
+        template_ps = sp.run(['bw', 'get', 'template', template_name],
+                             capture_output=True)
+        return json.loads(template_ps.stdout)
+
+    @staticmethod
+    def __get_items_by_search_pattern(search_pattern):
+        """Gets list of items that matches search pattern"""
+        completed_ps = sp.run(['bw', 'list', 'items'], capture_output=True)
+        items = json.loads(completed_ps.stdout)
+        if search_pattern:
+            items = list(
+                filter(
+                    lambda i: search_pattern.lower() in i['name'].lower(),
+                    items))
+        return items
